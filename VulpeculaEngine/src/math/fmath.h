625259e2a23a06f3bac0f21051af588c107e212f
torgaw <teo.carn@gmail.com>
2024-07-18 11:01:31 +0300

    lerp

--- VulpeculaEngine/src/math/fmath.h
+++ VulpeculaEngine/src/math/fmath.h
@@ -8,4 +8,7 @@ float fm_clamp_f(float d, float min, float max);
 int64_t fm_floor(double x);
 int32_t fm_floor_f(float x);
 
+double fm_lerp(double x, double y, double t);
+float fm_lerp_f(float x, float y, float t);
+
 #endif /* FMATH */
